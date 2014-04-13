406fd6a315669b915927a4b6d2845c71f7f1b499
Chongyuan <xiangcy@mit.edu>
2014-04-13 19:37:52 -0400

    add data.js

---
+++ js/data.js
@@ -0,0 +1,42 @@
+var blades = [
+  {
+    name: "Lucy's Blade",
+    price: 50.00,
+    brand: "Butterfly",
+    category: "Shakehand"
+  },
+  {
+    name: "Blue Thunder",
+    price: 60.95,
+    brand: "Avalox",
+    category: "Shakehand"
+  },
+  {
+    name: "Defence X",
+    price: 64.99,
+    brand: "Butterfly",
+    category: "Shakehand"
+  },
+  {
+    name: "Acoustic Penhold",
+    price: 159.95,
+    brand: "Nittaku",
+    category: "Penhold"
+  },
+  {
+    name: "Extra Special Penhold",
+    price: 74.95,
+    brand: "Butterfly",
+    category: "Penhold"
+  },
+  {
+    name: "Violin Japanese Rectangular",
+    price: 164.95,
+    brand: "Nittaku",
+    category: "Penhold"
+  }
+];
+
+var glues = [];
+
+var rubbers = [];
